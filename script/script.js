08f55d5cc840a1c401763618151386c70ad51242
Natalie <95322224+Nativ77@users.noreply.github.com>
2022-05-23 22:24:55 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -4,3 +4,10 @@ $('.about-items > div').on('click', function(){
     $(this).toggleClass('toggleColor');
     $(this).next('p').slideToggle(300).toggleClass('toggleColor')
 })
+
+$('feature-items').on('click', function(){
+    $(this).children('p').slideToggle(300);
+    $(this).children('.icon-feature').children('svg').children('path').toggleClass('feature-items__blue-image');
+    $(this).children('.icon-feature').children('span').toggleClass('change-color');
+    $(this).toggleClass('line-color');
+})
