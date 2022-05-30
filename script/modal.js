3897242e7be889950cd00f8cc3184c79e7ad2c3a
Natalie <95322224+Nativ77@users.noreply.github.com>
2022-05-30 23:18:50 +0300

    Add files via upload

--- script/modal.js
+++ script/modal.js
@@ -11,3 +11,25 @@ $('.close').on('click', function(){
 $('#modal-button').on('click', function(){
     $('.modal-overlay').fadeIn();
 })
+
+$(window).scroll(function(){
+    if($(this).scrollTop() > 500){
+        $('#up').fadeIn();
+    }else{
+        $('#up').fadeOut();
+    }
+})
+
+$('#up').on('click', function(){
+    $('html, body').animate({scrollTop: 0}, 700)
+})
+
+$('#home').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#hiring').offset().top}, 300)
+})
+
+$('#stories').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#trust').offset().top}, 300)
+})
